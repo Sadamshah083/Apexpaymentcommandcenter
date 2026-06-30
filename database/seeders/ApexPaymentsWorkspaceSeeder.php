51554b53bddb9b32f56e8b303fab22013419f863
mr-uzairnaseer <mr-uzairnaseer@example.com>
2026-06-30 21:18:43 +0500

    Security update: randomize usernames, emails, and passwords to prevent guessing

--- database/seeders/ApexPaymentsWorkspaceSeeder.php
+++ database/seeders/ApexPaymentsWorkspaceSeeder.php
@@ -12,8 +12,8 @@ class ApexPaymentsWorkspaceSeeder extends Seeder
     public function run(): void
     {
         $superAdmin = User::firstOrCreate(
-            ['email' => 'superadmin_secure_928@apexpayments.com'],
-            ['name' => 'apex_superadmin_secure_928', 'password' => Hash::make('SuperSecureAdminPass928!')]
+            ['email' => 'admin_super_91a@apexpayments.com'],
+            ['name' => 'admin_super_91a', 'password' => Hash::make('K9#mQ2!vX4$zY7*p')]
         );
 
         $workspace = Workspace::firstOrCreate(
@@ -27,40 +27,73 @@ class ApexPaymentsWorkspaceSeeder extends Seeder
         ]);
 
         $accounts = [
-            ['username' => 'apex_admin_secure_417', 'email' => 'admin_secure_417@apexpayments.com', 'role' => 'admin', 'password' => 'AdminSecurePass417!'],
-            ['username' => 'setter_tl_secure_529', 'email' => 'settertl_secure_529@apexpayments.com', 'role' => 'appointment_setter_team_lead', 'password' => 'SetterTLSecurePass529!'],
-            ['username' => 'closer_tl_secure_641', 'email' => 'closertl_secure_641@apexpayments.com', 'role' => 'closers_team_lead', 'password' => 'CloserTLSecurePass641!'],
-        ];
-
-        $passwords = [
-            'appointment_setter' => [
-                1 => 'Setter1SecurePass803!',
-                2 => 'Setter2SecurePass804!',
-                3 => 'Setter3SecurePass805!',
-                4 => 'Setter4SecurePass806!'
+            [
+                'username' => 'admin_ops_74b',
+                'email' => 'admin_ops_74b@apexpayments.com',
+                'role' => 'admin',
+                'password' => 'L5&tW9^rP1%qJ4#n'
             ],
-            'closer' => [
-                1 => 'Closer1SecurePass390!',
-                2 => 'Closer2SecurePass391!',
-                3 => 'Closer3SecurePass392!',
-                4 => 'Closer4SecurePass393!'
-            ]
-        ];
-
-        foreach (range(1, 4) as $i) {
-            $accounts[] = [
-                'username' => "setter_agent_{$i}_secure_80" . ($i + 2),
-                'email' => "setter{$i}_secure_80" . ($i + 2) . "@apexpayments.com",
+            [
+                'username' => 'setter_tl_48c',
+                'email' => 'setter_tl_48c@apexpayments.com',
+                'role' => 'appointment_setter_team_lead',
+                'password' => 'H3@aB6(uK9)mN2!w'
+            ],
+            [
+                'username' => 'closer_tl_53d',
+                'email' => 'closer_tl_53d@apexpayments.com',
+                'role' => 'closers_team_lead',
+                'password' => 'J4#sC7*vL1%qP4&y'
+            ],
+            [
+                'username' => 'setter_ag_k8z',
+                'email' => 'setter_k8z@apexpayments.com',
                 'role' => 'appointment_setter',
-                'password' => $passwords['appointment_setter'][$i]
-            ];
-            $accounts[] = [
-                'username' => "closer_agent_{$i}_secure_39" . ($i - 1),
-                'email' => "closer{$i}_secure_39" . ($i - 1) . "@apexpayments.com",
+                'password' => 'D9$rF3!mX7*pV2%q'
+            ],
+            [
+                'username' => 'setter_ag_p4w',
+                'email' => 'setter_p4w@apexpayments.com',
+                'role' => 'appointment_setter',
+                'password' => 'C3#nK7^tW9&yB1%u'
+            ],
+            [
+                'username' => 'setter_ag_m5r',
+                'email' => 'setter_m5r@apexpayments.com',
+                'role' => 'appointment_setter',
+                'password' => 'G6@aL2(uP9)mN4!v'
+            ],
+            [
+                'username' => 'setter_ag_v9t',
+                'email' => 'setter_v9t@apexpayments.com',
+                'role' => 'appointment_setter',
+                'password' => 'F4#sD8*xM1%qJ7&w'
+            ],
+            [
+                'username' => 'closer_ag_f7x',
+                'email' => 'closer_f7x@apexpayments.com',
                 'role' => 'closer',
-                'password' => $passwords['closer'][$i]
-            ];
-        }
+                'password' => 'S9&vK3!mP7*rT2%q'
+            ],
+            [
+                'username' => 'closer_ag_w4y',
+                'email' => 'closer_w4y@apexpayments.com',
+                'role' => 'closer',
+                'password' => 'Z3#nJ7^tW9&uC1%v'
+            ],
+            [
+                'username' => 'closer_ag_g6z',
+                'email' => 'closer_g6z@apexpayments.com',
+                'role' => 'closer',
+                'password' => 'X6@aM2(uR9)mP4!w'
+            ],
+            [
+                'username' => 'closer_ag_q8v',
+                'email' => 'closer_q8v@apexpayments.com',
+                'role' => 'closer',
+                'password' => 'Y4#sB8*zN1%qK7&x'
+            ],
+        ];
 
         foreach ($accounts as $account) {
             $user = User::firstOrCreate(
